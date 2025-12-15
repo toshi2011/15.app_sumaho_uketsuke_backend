208527c8e20fc61515feaa18fee80dcbc8f77cb1
toshi2011 <goul2007@gmail.com>
2025-12-16 08:33:21 +0900

    feat: テーブルAPIコントローラー、ルーター、サービスを追加し、tsconfig.jsonのフォーマットを調整する。

--- src/api/table/controllers/table.ts
+++ src/api/table/controllers/table.ts
@@ -4,4 +4,4 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::table.table');
+export default factories.createCoreController('api::table.table' as any);
