667f656e0976744f62a6acc4127bd66b80e7bf79
toshi2011 <goul2007@gmail.com>
2026-02-14 17:01:53 +0900

    プロジェクトルートおよび各コンポーネントディレクトリに散在していた一時ファイル、ログ、デバッグ用スクリプトの整理

--- check_env.js
+++
@@ -1,7 +0,0 @@
-const strapi = require('@strapi/strapi');
-const app = strapi().load().then(() => {
-    console.log('Database Client:', process.env.DATABASE_CLIENT);
-    console.log('Database Filename:', process.env.DATABASE_FILENAME);
-    console.log('Database Host:', process.env.DATABASE_HOST);
-    process.exit(0);
-});
